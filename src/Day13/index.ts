17b5bcd9df51d090f834c6031cb69839da15be48
Zajith Corro <zajithcorrov@hotmail.com>
2023-03-14 17:39:12 -0600

    :sparkles: Challenge Day 13

---
+++ src/Day13/index.ts
@@ -0,0 +1,11 @@
+import { doTask1, doTask2, doTask3 } from './tasks';
+
+export async function runCode() {
+	const strings: Array<String> = [];
+
+	strings.push(await doTask1());
+	strings.push(await doTask2());
+	strings.push(await doTask3());
+
+	return strings;
+}
